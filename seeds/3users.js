728491c7eb5cb08cdd3170f80a0b002411bc3c91
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-06-27 14:13:21 -0600

    seeds revamp

--- seeds/3users.js
+++ seeds/3users.js
@@ -1,11 +1,10 @@
-
 exports.seed = function(knex, Promise) {
-  return knex('bleed').del();
-  return knex('users').del().then(function () {
-      return Promise.join(
+  return knex('users').del()
+    .then(function () {
+      return Promise.all([
         knex('users').insert({id: 1, first_name: 'Nathan', last_name: 'Ostiguy', email: 'peterjostiguy@gmail.com', permission: true, birthday: '1987-10-11', diagnosis_id: 1, medicine_id: 2, telephone: '515-229-1737', address: '2938 Vallejo St, Denver, CO 80211'}),
         knex('users').insert({id: 2, first_name: 'Steven', last_name: 'Ostiguy', email: 'peterjostiguy@gmail.com', permission: false, birthday: '1987-10-11', diagnosis_id: 2, medicine_id: 4, telephone: '515-229-1737', address: '2938 Vallejo St, Denver, CO 80211'}),
         knex('users').insert({id: 3, first_name: 'Elaine', last_name: 'Ostiguy', email: 'peterjostiguy@gmail.com', permission: true, birthday: '1987-10-11', diagnosis_id: 4, medicine_id: 5, telephone: '515-229-1737', address: '2938 Vallejo St, Denver, CO 80211'})
-      );
+      ]);
     });
 };
