6265efadf6a19298b2f2fabb1b2e1e71b5e535ce
peterjostiguy <peterjostiguy@gmail.com>
2016-06-29 16:10:54 -0600

    adding design and maybe bradford

--- routes/patient.js
+++ routes/patient.js
@@ -4,12 +4,17 @@ var auth = require('../auth');
 var db = require('../db/api');
 var twilio = require('../twilio');
 require('dotenv').load();
-
+var splice = require('string-splice');
 
 // GET patient home page, must be authenticated to view
 router.get('/', auth.ensureAuthenticated, auth.isPatient, function(request, response, next) {
   db.findUserById(request.user.id).then(function(user) {
     db.findBleedIncident(user.user_id).then(function(bleed) {
+      // Format each bleed event date
+      bleed.forEach(function(bleedEventObj) {
+        var string = bleedEventObj.event_date.toString();
+        bleedEventObj.event_date = splice(string, 16, 24);
+      });
       response.render('patient', { dbUser: user,
                                    googleUser: request.user,
                                    bleed: bleed
