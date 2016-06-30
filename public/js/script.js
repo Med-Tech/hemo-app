e634f805a48026b47f6066984e77d98eda980264
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-06-29 22:47:27 -0600

    all bleed edit events pre-populating

--- public/js/script.js
+++ public/js/script.js
@@ -26,21 +26,27 @@ $(document).ready(function(){
 
   // Triggered when modal is about to be shown
   $('#editModal').on('show.bs.modal', function(event) {
-      // Get data-id attribute of the clicked element
-      var event_date = $(event.relatedTarget).data('event-date');
-      var description = $(event.relatedTarget).data('description');
-      var physical_location = $(event.relatedTarget).data('physical-location');
-      var dose = $(event.relatedTarget).data('dose');
-      var id = $(event.relatedTarget).data('bleed-id');
-      // Format event date with slice and moment.js
-      var dateStr = event_date.toString().slice(3);
-      event_date = moment(dateStr, 'MMM-DD-YYYY').format('YYYY-MM-DD');
-      // Populate the input area
-      $(event.currentTarget).find('input[name=event_date]').val(event_date);
-      $(event.currentTarget).find('input[name="description"]').val(description);
-      $(event.currentTarget).find('input[name="physical_location"]').val(physical_location);
-      $(event.currentTarget).find('input[name="dose"]').val(dose);
-      $(event.currentTarget).find('input[name="bleed_id"]').val(id);
+    var relatedTarget = $(event.relatedTarget);
+    var currentTarget = $(event.currentTarget);
+    // Get data-id attribute of the clicked element
+    var event_date = relatedTarget.data('event-date');
+    var description = relatedTarget.data('description');
+    var physical_location = relatedTarget.data('physical-location');
+    var medicine = relatedTarget.data('medicine');
+    var dose = relatedTarget.data('dose');
+    var prioritize = relatedTarget.data('prioritize');
+    var id = relatedTarget.data('bleed-id');
+    // Format event date with slice and moment.js
+    var dateStr = event_date.toString().slice(3);
+    event_date = moment(dateStr, 'MMM-DD-YYYY').format('YYYY-MM-DD');
+    // Populate the input area
+    currentTarget.find('input[name=event_date]').val(event_date);
+    currentTarget.find('input[name="description"]').val(description);
+    currentTarget.find('input[name="physical_location"]').val(physical_location);
+    currentTarget.find('select[name="medicine"]').find("option[value='" + medicine + "']").prop('selected', 'selected');
+    currentTarget.find('input[name="dose"]').val(dose);
+    currentTarget.find("input[value='" + prioritize + "']").prop('checked', true);
+    currentTarget.find('input[name="bleed_id"]').val(id);
   });
 
 });
