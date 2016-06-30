cc81a1cbd62a01e0224df6b0ca6eac1757af79a0
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-06-30 14:46:28 -0600

    add working verify button

--- public/js/script.js
+++ public/js/script.js
@@ -1,26 +1,30 @@
 $(document).ready(function(){
 
-  $('#patientselect').on('click', function(){
+  $('#patientselect').on('click', function() {
     $('.patientinfo').show();
     $('.providerpassword').hide();
     $('#profilesubmit').removeAttr('disabled');
     $('#profilesubmit').css('color','black');
   });
 
-  $('#providerselect').on('click', function(){
+  $('#providerselect').on('click', function() {
     $('.providerpassword').show();
     $('.patientinfo').hide();
     $('#profilesubmit').attr('disabled','disabled');
     $('#profilesubmit').css('color','gray');
   });
 
-  $('#verify').on('click', function(){
+  $('#providerpassword').keyup(function() {
     if($('#providerpassword').val()== 1234){
-      $('#profilesubmit').removeAttr('disabled');
+      $('#profilesubmit').prop('disabled', false);
       $('#profilesubmit').css('color','black');
-    }
-    else {
-      alert('Invalid Verification Code. If you are a patient, please select "Patient" above. If you are a provider, contact your IT department for the correct access code.');
+      $('#providerpassword').removeClass('red-border');
+      $('#providerpassword').addClass('green-border');
+    } else {
+      $('#profilesubmit').attr('disabled', true);
+      $('#profilesubmit').css('color','grey');
+      $('#providerpassword').removeClass('green-border');
+      $('#providerpassword').addClass('red-border');
     }
   });
 
