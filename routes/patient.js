46bd659f59448fbfc2b7db653e3a8b6d008cfc50
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-07-01 13:49:11 -0600

    final project for presentation

--- routes/patient.js
+++ routes/patient.js
@@ -33,23 +33,11 @@ router.post('/create', auth.ensureAuthenticated, auth.isPatient, function(reques
         twilio.sendMessage('4843547333', urgent);
         twilio.sendMessage('3035478715', urgent);
         twilio.sendMessage('9252853344', urgent);
-        // twilio.sendMessage('6157147175', urgent);
-        // twilio.sendMessage('3035976996', urgent);
-        // twilio.sendMessage('8172287173', urgent);
-        // twilio.sendMessage('7203454678', urgent);
-        // twilio.sendMessage('7205395828', urgent);
-        // twilio.sendMessage('3037209905', urgent);
       } else {
         twilio.sendMessage('5152291737', non_urgent);
         twilio.sendMessage('4843547333', non_urgent);
         twilio.sendMessage('3035478715', non_urgent);
         twilio.sendMessage('9252853344', non_urgent);
-        // twilio.sendMessage('6157147175', non_urgent);
-        // twilio.sendMessage('3035976996', non_urgent);
-        // twilio.sendMessage('8172287173', non_urgent);
-        // twilio.sendMessage('7203454678', non_urgent);
-        // twilio.sendMessage('7205395828', non_urgent);
-        // twilio.sendMessage('3037209905', non_urgent);
       }
     });
   response.redirect('/patient');
