449e201bbab6f687134581e654b31a289e16f024
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-07-01 10:24:28 -0600

    add phone numbers for presentation, fix hemophilapp nav button

--- routes/patient.js
+++ routes/patient.js
@@ -26,18 +26,32 @@ router.get('/', auth.ensureAuthenticated, auth.isPatient, function(request, resp
 // POST to /create route to add bleed event
 router.post('/create', auth.ensureAuthenticated, auth.isPatient, function(request, response, next) {
   db.insertBleedEvent(request.body).then(function() {
-    if (request.body.prioritize === 'true') {
-      twilio.sendMessage('5152291737', 'An urgent message has been posted! Please reply immediately.');
-      twilio.sendMessage('4843547333', 'An urgent message has been posted! Please reply immediately.');
-      twilio.sendMessage('3035478715', 'An urgent message has been posted! Please reply immediately.');
-      twilio.sendMessage('9252853344', 'An urgent message has been posted! Please reply immediately.');
-    } else {
-      twilio.sendMessage('5152291737', 'A non-urgent message has been posted. Please reply soon.');
-      twilio.sendMessage('4843547333', 'A non-urgent message has been posted. Please reply soon.');
-      twilio.sendMessage('3035478715', 'A non-urgent message has been posted. Please reply soon.');
-      twilio.sendMessage('9252853344', 'A non-urgent message has been posted. Please reply soon.');
-    }
-  });
+    var urgent = 'An urgent message has been posted! Please reply immediately.';
+    var non_urgent = 'A non-urgent message has been posted. Please reply soon.';
+      if (request.body.prioritize === 'true') {
+        twilio.sendMessage('5152291737', urgent);
+        twilio.sendMessage('4843547333', urgent);
+        twilio.sendMessage('3035478715', urgent);
+        twilio.sendMessage('9252853344', urgent);
+        // twilio.sendMessage('6157147175', urgent);
+        // twilio.sendMessage('3035976996', urgent);
+        // twilio.sendMessage('8172287173', urgent);
+        // twilio.sendMessage('7203454678', urgent);
+        // twilio.sendMessage('7205395828', urgent);
+        // twilio.sendMessage('3037209905', urgent);
+      } else {
+        twilio.sendMessage('5152291737', non_urgent);
+        twilio.sendMessage('4843547333', non_urgent);
+        twilio.sendMessage('3035478715', non_urgent);
+        twilio.sendMessage('9252853344', non_urgent);
+        // twilio.sendMessage('6157147175', non_urgent);
+        // twilio.sendMessage('3035976996', non_urgent);
+        // twilio.sendMessage('8172287173', non_urgent);
+        // twilio.sendMessage('7203454678', non_urgent);
+        // twilio.sendMessage('7205395828', non_urgent);
+        // twilio.sendMessage('3037209905', non_urgent);
+      }
+    });
   response.redirect('/patient');
 });
 
